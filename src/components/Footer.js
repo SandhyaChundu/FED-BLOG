ffa8e448479857bac84e7a282de221a96a5e01ac
SandhyaChundu <110108826+SandhyaChundu@users.noreply.github.com>
2022-10-17 21:58:40 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="footer">
         <div className="footer-content">
                   <h3>My FED Blog</h3>
-                  <p>Page Credits : GADDAM SHARMILA ~20BQ1A0554</p>
+                  <p>Page Credits :CHUNDU SANDHYA ~20BQ1A0544</p>
             <p>copyright &copy; {new Date().getFullYear()} <a href=""> FED Blog</a>  </p>
         </div>
       </div>
